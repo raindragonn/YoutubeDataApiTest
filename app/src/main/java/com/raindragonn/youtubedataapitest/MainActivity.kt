5805db001545e141a60f8062a1505d8a6a15f5f8
raindragonn <raindragonn@gmail.com>
2023-12-08 23:13:41 +0900

    feat: API KEY를 이용한 검색 구현

--- app/src/main/java/com/raindragonn/youtubedataapitest/MainActivity.kt
+++ app/src/main/java/com/raindragonn/youtubedataapitest/MainActivity.kt
@@ -1,25 +1,15 @@
 package com.raindragonn.youtubedataapitest
 
-import android.accounts.AccountManager
-import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.edit
 import androidx.lifecycle.lifecycleScope
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
-import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.util.ExponentialBackOff
 import com.google.api.services.youtube.YouTube
-import com.google.api.services.youtube.YouTubeScopes
 import com.google.api.services.youtube.model.SearchResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -27,8 +17,7 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        const val PREF_NAME = "PREF_NAME"
-        const val KEY_SELECTED_NAME = "KEY_SELECTED_NAME"
+        const val YOUTUBE_DATA_API_KEY = "Youtube Data API 키"
     }
 
     private lateinit var _etSearch: EditText
@@ -38,16 +27,6 @@ class MainActivity : AppCompatActivity() {
     private val searchText
         get() = _etSearch.text.toString()
 
-    private val _credential: GoogleAccountCredential by lazy {
-        GoogleAccountCredential.usingOAuth2(
-            applicationContext,
-            listOf(YouTubeScopes.YOUTUBE_READONLY),
-        ).setBackOff(ExponentialBackOff())
-    }
-
-    private val _credentialLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult(), ::credentialCallBack
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,44 +42,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun search() {
-        if (checkSignIn()) {
-            lifecycleScope.launch {
-                val result = youtubeSearch(searchText).joinToString("\n") { it.snippet.title }
-                _tvResult.text = result
-            }
+        lifecycleScope.launch {
+            val result = youtubeSearch(searchText).joinToString("\n") { it.snippet.title }
+            _tvResult.text = result
         }
-    }
-
-    private fun checkSignIn(): Boolean {
-        val lastSelectedAccountName = getSharedPreferences(PREF_NAME, MODE_PRIVATE)
-            .getString(KEY_SELECTED_NAME, null)
-        return if (lastSelectedAccountName.isNullOrBlank()) {
-            val signInIntent = _credential.newChooseAccountIntent()
-            _credentialLauncher.launch(signInIntent)
-            false
-        } else {
-            _credential.selectedAccountName = lastSelectedAccountName
-            true
-        }
-    }
-
-    private fun credentialCallBack(result: ActivityResult) {
-        val accountName = result.data?.getStringExtra(AccountManager.KEY_ACCOUNT_NAME) ?: return
-
-        getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE).edit {
-            putString(KEY_SELECTED_NAME, accountName)
-        }
-        _credential.selectedAccountName = accountName
-
-        Toast.makeText(this, "검색 가능", Toast.LENGTH_SHORT).show()
     }
 
     private suspend fun youtubeSearch(query: String): List<SearchResult> {
         val transport = NetHttpTransport()
         val jsonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
         val youtube = YouTube.Builder(
-            transport, jsonFactory, _credential
-        ).setApplicationName(getString(R.string.app_name))
+            transport, jsonFactory,
+        ) {}.setApplicationName(getString(R.string.app_name))
             .build()
 
         return withContext(Dispatchers.IO) {
@@ -109,6 +62,7 @@ class MainActivity : AppCompatActivity() {
                 val parts = listOf("id", "snippet").joinToString()
                 val searchList =
                     youtube.search().list(parts).apply {
+                        key = YOUTUBE_DATA_API_KEY
                         q = query
                         type = "video"
                         // 음악 카테고리 참고 [https://gist.github.com/dgp/1b24bf2961521bd75d6c]
@@ -116,17 +70,10 @@ class MainActivity : AppCompatActivity() {
                     }
                 val response = searchList?.execute()
                 response?.items ?: listOf()
-            }.recover { throwable ->
-                if (throwable is UserRecoverableAuthIOException) {
-                    launch(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, "인증이 필요합니다.", Toast.LENGTH_SHORT).show()
-                        // 엑세스 권한이 없는 경우 권한 요청을 위한 Intent
-                        val accessRequestIntent = throwable.intent
-                        _credentialLauncher.launch(accessRequestIntent)
-                    }
-                }
+            }.recover {
+                it.printStackTrace()
                 listOf()
-            }.getOrNull() ?: listOf()
+            }.getOrElse { listOf() }
         }
     }
 
